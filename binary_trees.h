eea0d7d84b961a432fda6b4d99dcd58daf969020
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-25 13:47:35 +0000

    New node

--- binary_trees.h
+++ binary_trees.h
@@ -15,6 +15,7 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 /* Functions */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
