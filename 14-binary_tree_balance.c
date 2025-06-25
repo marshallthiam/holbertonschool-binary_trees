7aa78145a94096225d252055551ca4408492c1b4
marshallthiam <marshallthiam@holbertonstudents.com>
2025-06-25 12:39:12 +0000

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_height;
+    size_t right_height;
+
     if (tree == NULL)
         return (0);
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
     return (1 + (left_height > right_height ? left_height : right_height));
 }
@@ -25,11 +28,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    int left_height;
+    int right_height;
+
     if (tree == NULL)
         return (0);
 
-    int left_height = (int)binary_tree_height(tree->left);
-    int right_height = (int)binary_tree_height(tree->right);
+    left_height = (int)binary_tree_height(tree->left);
+    right_height = (int)binary_tree_height(tree->right);
 
     return (left_height - right_height);
 }
